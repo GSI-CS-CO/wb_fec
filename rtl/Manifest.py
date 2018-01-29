45e933e864ac0a0251daea35c35d66fae2bee46b
Cesar Prados <c.prados@gsi.de>
2018-01-29 01:39:37 +0100

    wb_fec: add a modified wrf_loopback module

--- rtl/Manifest.py
+++ rtl/Manifest.py
@@ -1,6 +1,7 @@
 modules =  {
     "local" : [
     "erasure_channel",
+    "xwrf_fec_loopback",
     "wrf_pkt_dropper"]
 }
 
---
+++ rtl/xwrf_fec_loopback/Manifest.py
@@ -0,0 +1,5 @@
+files = [   "lbk_fec_pkg.vhd",
+            "lbk_fec_wishbone_controller.vhd",
+            "lbk_fec_wishbone.wb",
+            "wrf_fec_loopback.vhd",
+            "xwrf_fec_loopback.vhd"];
